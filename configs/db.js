c673a67fc213a03646bee7450ab1e8824fea2163
salveketan <salveketan6@gmail.com>
2022-11-19 23:32:23 +0530

    nodemon install

--- configs/db.js
+++ configs/db.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/preflight").then(() => {
+mongoose.connect("mongodb://ketan:ketan123@ac-9qijpz6-shard-00-00.ixojjet.mongodb.net:27017,ac-9qijpz6-shard-00-01.ixojjet.mongodb.net:27017,ac-9qijpz6-shard-00-02.ixojjet.mongodb.net:27017/?ssl=true&replicaSet=atlas-aq08k0-shard-0&authSource=admin&retryWrites=true&w=majority").then(() => {
     console.log("database connected");
 }).catch(() => {
     console.log("database not connected");
 })
+
+
+// mongodb://ketan:<password>@ac-9qijpz6-shard-00-00.ixojjet.mongodb.net:27017,ac-9qijpz6-shard-00-01.ixojjet.mongodb.net:27017,ac-9qijpz6-shard-00-02.ixojjet.mongodb.net:27017/?ssl=true&replicaSet=atlas-aq08k0-shard-0&authSource=admin&retryWrites=true&w=majority

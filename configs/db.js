5fcff1108f93d5b74fe3cee87ed2d31cc2787833
salveketan <salveketan6@gmail.com>
2022-11-20 10:05:47 +0530

    new changes

--- configs/db.js
+++ configs/db.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://ketan:ketan123@ac-9qijpz6-shard-00-00.ixojjet.mongodb.net:27017,ac-9qijpz6-shard-00-01.ixojjet.mongodb.net:27017,ac-9qijpz6-shard-00-02.ixojjet.mongodb.net:27017/?ssl=true&replicaSet=atlas-aq08k0-shard-0&authSource=admin&retryWrites=true&w=majority").then(() => {
+mongoose.connect("mongodb+srv://ketan:ketan123@cluster0.ixojjet.mongodb.net/preflight").then(() => {
     console.log("database connected");
-}).catch(() => {
-    console.log("database not connected");
+}).catch((e) => {
+    console.log({ message: e.message });
 })
 
 
 // mongodb://ketan:<password>@ac-9qijpz6-shard-00-00.ixojjet.mongodb.net:27017,ac-9qijpz6-shard-00-01.ixojjet.mongodb.net:27017,ac-9qijpz6-shard-00-02.ixojjet.mongodb.net:27017/?ssl=true&replicaSet=atlas-aq08k0-shard-0&authSource=admin&retryWrites=true&w=majority
+
+
+// mongodb + srv://ketan:ketan123@cluster0.ixojjet.mongodb.net/test
